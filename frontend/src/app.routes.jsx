83a2e330fe1ccea5e36843f317ebaa679ba5c74d
ishandrai07 <mywork4ever.77@gmail.com>
2026-08-04 13:13:24 +0530

    Home and Interview page design

--- frontend/src/app.routes.jsx
+++ frontend/src/app.routes.jsx
@@ -17,5 +17,9 @@ export const router = createBrowserRouter([
     {
         path:"/",
         element:<Protected><Home /></Protected>
+    },
+    {
+        path:"/interview/:interviewId",
+        element: <Protected><Interview /></Protected>
     }
 ])
