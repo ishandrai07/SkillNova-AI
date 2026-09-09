caaaf2fd3170ed52e16038eb7df4b1c7b8dcc78c
ishand <mywork4ever.77@gmail.com>
2026-09-09 23:43:27 +0530

    refactor

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,39 +1,34 @@
-const express = require('express')
+const express = require("express");
 
-const authRouter = express.Router() 
-const authContoller = require('../controllers/auth.controller')
-const authMiddleware = require('../middlewares/auth.middleware')
-const { authRateLimiter } = require('../middlewares/rateLimit.middleware')
-/**
- * @route POST/api/auth/regiter
- * @description Register a new user
- * @access Public
- */
+const authRouter = express.Router();
 
-authRouter.post("/register", authRateLimiter, authContoller.registerUserController)
+const authController = require("../controllers/auth.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
+const {
+    authRateLimiter
+} = require("../middlewares/rateLimit.middleware");
 
-/**
- * @route POST/api/auth/login
- * @description login user with email and password
- * @access Public
- */
+authRouter.post(
+    "/register",
+    authRateLimiter,
+    authController.registerUserController
+);
 
-authRouter.post("/login", authRateLimiter, authContoller.loginUserController)
+authRouter.post(
+    "/login",
+    authRateLimiter,
+    authController.loginUserController
+);
 
+authRouter.get(
+    "/logout",
+    authController.logoutUserController
+);
 
-/**
- * @route GET/api/auth/logout
- * @description clear token from user cookie and add the token in blacklist
- * @access public
- */
+authRouter.get(
+    "/get-me",
+    authMiddleware.authUser,
+    authController.getMeController
+);
 
-authRouter.get("/logout", authContoller.logoutUserController)
-
-/**
- * @route GET/api/auth/get-me
- * @description gte the current logged in user details
- * @access private
- */
-authRouter.get("/get-me", authMiddleware.authUser, authContoller.getMeController)
-
-module.exports = authRouter
+module.exports = authRouter;
